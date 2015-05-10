005555785b2f26d95a7bdbb1a0532a4c8d904fb6
Prelang Builder <builder@prelang.com>
2015-05-10 20:19:05 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Ease' to allow for create, update

--- app/admin/ease.rb
+++ app/admin/ease.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Ease do
+  permit_params :name, :description, :icon, :image, :published, :heroclass
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
