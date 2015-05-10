fe811378df51d40e82fe282f2d32350ada7cc729
Prelang Builder <builder@prelang.com>
2015-05-10 20:17:45 +0000

    Generated model Diet with migration

---
+++ db/migrate/20150510201745_create_diets.rb
@@ -0,0 +1,14 @@
+class CreateDiets < ActiveRecord::Migration
+  def change
+    create_table :diets do |t|
+      t.string :name
+      t.text :description
+      t.string :icon
+      t.string :image
+      t.boolean :published
+      t.string :heroclass
+
+      t.timestamps
+    end
+  end
+end
