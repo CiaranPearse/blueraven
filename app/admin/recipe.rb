c87d04a08534506b634a5f012c8e3ac267cd98ec
Prelang Builder <builder@prelang.com>
2015-05-10 20:19:46 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Recipe' to allow for create, update

--- app/admin/recipe.rb
+++ app/admin/recipe.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Recipe do
+  permit_params :title, :description, :image, :ingredients, :imagepath, :imagecredit, :yeild, :preptime, :cooktime, :totaltime, :rating, :commentnumber, :flikes, :tlikes, :plikes, :slikes, :glikes, :pubdate, :author, :source, :published, :calories, :caoriesfromfat, :totalfat, :totalfatper, :saturatedfar, :saturatedfatper, :transfat, :cholesterol, :cholesterolper, :sodium, :sodiumper, :carbs, :carbsper, :dietaryfiber, :dietaryfiberper, :sugars, :protein, :vitamina, :vitaminc, :calcium, :iron, :yumms, :shares
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
